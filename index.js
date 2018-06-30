7d4826959ddd2ef893d56f0162804ef25e1bcc04
jonahthompson <jonahthompson12@gmail.com>
2018-06-30 17:15:40 -0600

    new background and color scheme

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(function() {
-	// window.sr = ScrollReveal({ reset: true });
-	// sr.reveal('.content-wrap', { opacity: 0, origin: 'top', scale: 1 });
+	window.sr = ScrollReveal({ reset: false });
+	sr.reveal('.parallax', { duration: 1500, origin: 'top', scale: 1, distance: '10px', easing: 'cubic-bezier(0.46, 0, 0.07, 1)'});
+
+	// $('.navbar').slideRight();
 });
